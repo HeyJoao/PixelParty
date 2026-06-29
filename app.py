d6d375bff04c488b51da7afd7b4c0d067cc7ff77
João Pedro Chagas <dasilveirachagasjoaopedro@gmail.com>
2026-06-29 00:03:08 -0300

    atualização

--- app.py
+++ app.py
@@ -422,4 +422,4 @@ def on_palpite(dados):
 if __name__ == "__main__":
     porta = int(os.environ.get("PORT", 5000))
     print("Lendo o bloco principal...")
-    socketio.run(app, host="0.0.0.0", port=porta)
+    socketio.run(app, host="0.0.0.0", port=porta, allow_unsafe_werkzeug=True)
